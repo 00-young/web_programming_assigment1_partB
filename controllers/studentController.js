aa5cad0f2f090b0e5142808a3653ca15c9cebee6
SeoYoung Oh <myme0722@gachon.ac.kr>
2026-03-31 13:57:43 +0900

    commit 4:modify POST array input

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,6 +3,13 @@ const Student = require('../models/student');
 
 exports.addStudent = (req, res) => {
     const data = req.body;
+    if (Array.isArray(data)) {
+      const newStudents = data.map(item =>
+        new Student(item.name, item.age, item.id)
+      );
+      students.push(...newStudents);
+      return res.json(newStudents);
+    }
     const student = new Student(data.name, data.age, data.id);
     students.push(student);
     res.json(student);
